a8ef0472f48a07749a633f9d4adbcc4abeccc913
SANJOSE MARY - sanjosemary904@gmail.com <sanjosemary904@vlsi38.ee.iitb.ac.in>
2019-07-29 14:06:38 +0530

    Add abi_codes

--- abi_codes/ad_2_num/compile_for_ajit.sh
+++
@@ -1,6 +0,0 @@
-genVmapAsm vmap.txt setup_page_tables.s
-compileToSparc.py -g -I /usr/include -I ./ -s init.s -s setup_page_tables.s  -s $AJIT_TOOLS_RELEASE/tools/ajit_access_routines/asm/trap_handlers.s -I $AJIT_TOOLS_RELEASE/tools/ajit_access_routines/include -C $AJIT_TOOLS_RELEASE/tools/ajit_access_routines/src -C $AJIT_TOOLS_RELEASE/tools/minimal_printf_timer/src -I $AJIT_TOOLS_RELEASE/tools/minimal_printf_timer/include -c main.c -E hello.elf -V hello.vars -H hello.hex -M hello.mmap -O hello.objdump  -L LinkerScript.lnk  -D AJIT 
-echo "\nREADELF\n"
-sparc-linux-readelf  --hex-dump=1 --hex-dump=2 --hex-dump=3  --hex-dump=.bss hello.elf | grep 0x > hello.hex 
-generateMemoryMap_Byte hello.hex > hello.mmap
-remapMemmap vmap.txt hello.mmap hello.mmap.remapped
--- abi_codes/float_add/main.c~
+++
@@ -1,6 +0,0 @@
-float add(float a, float b);
-
-int main() {
-  add(10.5, 12.5);
-  return 0;
-}
