f6242c7cbce62d880315be134c7cf11d5f5661ed
GauriP19 <53228858+GauriP19@users.noreply.github.com>
2019-07-30 16:28:25 +0530

    Update add.s

--- abi_codes/add_fp_num/add.s
+++ abi_codes/add_fp_num/add.s
@@ -4,7 +4,7 @@
 .type add, #function
 
 add:
-        save  %sp, -96, %sp
+        save  %sp, -80, %sp
         ld [%fp+100], %f2    !The compiler stores fp numbers in memoery stack of caller.
         ld [%fp+96], %f1     !The numbers are loaded from that adress.
         fadds %f2,%f1,%f0    !fp regosters are visible to caller and callee at al times, hence caller ill find at fo itself
