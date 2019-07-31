f97eee067c2849c0519a8b879551e43444a97194
GauriP19 <gauripatrikar@gmail.com>
2019-07-31 06:39:33 -0400

    SIMD2 changes3.1

--- New_Instn/Vector/SIMD2/zero/zero.s
+++ New_Instn/Vector/SIMD2/zero/zero.s
@@ -4,11 +4,11 @@
 
 main:
 start: 
-	mov 240,%g2
-	mov 255,%g3
-	mov 34,%g4
-	zbytedpos %g2,%g4,%g6
-	zbytedpos %g2, 3,%g6
+	mov 0x1,%g2              !input in 64 bit register
+	set 0x01010101,%g3
+	mov 0xfb,%g4             !mask
+	zbytedpos %g2,%g4,%g6    !g6 = 0x 00011011
+	zbytedpos %g2, 0xf1,%l0  !l0 = 0x 00010001
 
 
 
