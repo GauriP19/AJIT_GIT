79b1b557a0431982cc9eb2ae2a512b6ffa4fda5d
GauriP19 <gauripatrikar@gmail.com>
2019-07-31 06:12:12 -0400

    SIMD2 changes2

--- New_Instn/Vector/SIMD2/or/or.s
+++ New_Instn/Vector/SIMD2/or/or.s
@@ -5,7 +5,7 @@
 main:
 start: 
 	mov 0x1,%g2           !input to register pair
-	set 0x08050403,%g3
+	set 0x08050401,%g3
 	mov 0x1b ,%g4         !mask
 	ordbyter %g2,%g4,%g6  !g6 = d
 
--- New_Instn/Vector/SIMD2/xor/xor.s
+++ New_Instn/Vector/SIMD2/xor/xor.s
@@ -4,10 +4,10 @@
 
 main:
 start: 
-	mov 240,%g2
-	mov 255,%g3
-	mov 34,%g4
-	xordbyter %g2,%g4,%g6
+	mov 0x1,%g2             !Input number in register pair
+	set 0x08050401,%g3
+	mov 0x1b,%g4            !mask
+	xordbyter %g2,%g4,%g6   !g6 = c
 
 
 
