4768890df438648305f1a718071385cc0766c13d
GauriP19 <gauripatrikar@gmial.com>
2019-08-07 05:26:10 -0400

    merge

--- New_Instn/Vector/SIMD2/zero/zero.s
+++ New_Instn/Vector/SIMD2/zero/zero.s
@@ -7,9 +7,7 @@ start:
 	mov 0x1,%g2              !input in 64 bit register
 	set 0x01010101,%g3
 	mov 0xfb,%g4             !mask
-	zbytedpos %g2,%g4,%g6    !g6 = 0x 00011011
-	zbytedpos %g2, 0xf1,%l0  !l0 = 0x 00010001
-
-
+	zbytedpos %g2,%g4,%g6    !g6 = 0xe0
+	zbytedpos %g2, 0xf1,%g7  !l0 = 0xe0
 
 end:	ta 0
