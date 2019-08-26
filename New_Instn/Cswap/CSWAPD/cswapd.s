8aebee541d6d3dbdde51b7c36572c5958bb8941a
GauriP19 <gauripatrikar@gmail.com>
2019-08-26 07:23:39 -0400

    new_model_test

--- New_Instn/Cswap/CSWAPD/cswapd.s
+++ New_Instn/Cswap/CSWAPD/cswapd.s
@@ -2,18 +2,21 @@
 .text
 main:
 start:
-	set 0x3,%g2
-	set 0x4,%g3
+	set 0x0,%g2
+	set 0x3,%g3
 	std %g2, [%fp + -8]   !Store values to be exchanged in memory
 	
+	mov %fp, %g2
+	add %g2, -8, %g2
+	
+
 	set 0x5,%g4
 	set 0x6,%g5
 
 #	set 0xfffffff8, %g6
 
-	cswapd [%fp+ -8],%g4  !After this, g4, g5  = 3, 4 
-
-#	cswapda [%g6] 0xb,%g4
+	cswapd [%fp + -8] ,%g4  !After this, g4, g5  = 3, 4 
+#	cswapda [%fp + -4],%g4
 	
 
 end: 	ta 0
